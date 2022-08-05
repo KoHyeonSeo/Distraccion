2e3b30b2912c2d02b031afda45749579aab57ce8
KoHyeonSeo <76097749+KoHyeonSeo@users.noreply.github.com>
2022-08-05 11:06:46 +0900

    Add EnemyMission2 Structure

---
+++ Assets/Script/Entity/Enemy/Test/Test2/TestMissionComplete2.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(menuName = "MissionComplete/test2")]
+public class TestMissionComplete2 : MissionComplete
+{
+    public override void MissionCompleteSetting()
+    {
+
+    }
+}
