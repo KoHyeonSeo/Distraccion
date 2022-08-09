45dace3f639cc1a86d510ff06227bd72031bc43c
Kohyeonseo <rhgustj01@naver.com>
2022-08-09 21:22:20 +0900

    Update Item

--- Assets/Script/Entity/Player/PlayerMove.cs
+++ Assets/Script/Entity/Player/PlayerMove.cs
@@ -330,9 +330,10 @@ public class PlayerMove : MonoBehaviour
         {
             for (int i=0; i < GameManager.Instance.ItemProp.Count; i++)
             {
-                if (GameManager.Instance.ItemProp[i].Item == other.gameObject)
+                if (GameManager.Instance.ItemProp[i].Item.name.Contains(other.gameObject.name))
                 {
-                    //GameManager.Instance.ItemProp[i].isHaveItem = true;
+                    GameManager.Instance.SetHaveItem = i;
+                    Destroy(other.gameObject);
                     return;
                 }
             }
