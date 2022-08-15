b1a737853b8041840ee4e730f61a9e5798350527
Kohyeonseo <rhgustj01@naver.com>
2022-08-15 22:36:48 +0900

    Update Stege1.5

--- Assets/Script/Entity/Player/MiddleStagePlayerMove.cs
+++ Assets/Script/Entity/Player/MiddleStagePlayerMove.cs
@@ -5,17 +5,22 @@ using UnityEngine;
 public class MiddleStagePlayerMove : MonoBehaviour
 {
     private PlayerInput playerInput;
-
+    private Rigidbody2D rb;
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpForce = 10f;
     private void Start()
     {
         playerInput = GetComponent<PlayerInput>();  
+        rb = GetComponent<Rigidbody2D>(); 
     }
     private void Update()
     {
         //SpaceBar¿ª ¥≠∑∂¿ª ∂ß Jump
         if (playerInput.UseItemButton)
         {
-
+            rb.AddForce(Vector2.up * jumpForce);
         }
+        Vector3 dir = Vector3.right * playerInput.XKeyBoardAxis;
+        transform.position += dir * speed * Time.deltaTime;
     }
 }
---
+++ Assets/Script/Other/Mic.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Mic : MonoBehaviour
+{
+    //public Slider sound_slider;
+    public AudioClip clip;
+    int sampleRate = 44100;
+    private float[] samples;
+    public float rmsValue;
+    public float modulate = 2000;
+    public int resultValue;
+    public int cutValue = 15;
+
+    public int Volume { get; private set; }
+    private void Start()
+    {
+        samples = new float[sampleRate];
+        clip = Microphone.Start(Microphone.devices[0].ToString(), true, 1, sampleRate);
+    }
+
+    private void Update()
+    {
+        //offset은 시작 위치
+        clip.GetData(samples, 0); //-1f ~ 1f
+        float sum = 0;
+        for (int i = 0; i < samples.Length; i++)
+        {
+            //sum += Mathf.Abs(samples[i]);
+            sum += samples[i] * samples[i];
+        }
+        rmsValue = Mathf.Sqrt(sum / samples.Length);
+        rmsValue *= modulate;
+        rmsValue = Mathf.Clamp(rmsValue, 0, 100);
+        resultValue = Mathf.RoundToInt(rmsValue);
+        if (resultValue < cutValue)
+        {
+            resultValue = 0;
+        }
+        Volume = resultValue;
+    }
+
+}
