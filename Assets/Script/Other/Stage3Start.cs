020673e76f93f4f5e6a1075d910e6dc50782449a
suhyun <86099781+Suhyun77@users.noreply.github.com>
2022-08-17 05:51:26 +0900

    Update Stage3Start.cs

--- Assets/Script/Other/Stage3Start.cs
+++ Assets/Script/Other/Stage3Start.cs
@@ -34,22 +34,26 @@ public class Stage3Start : MonoBehaviour
         topButton.localEulerAngles = new Vector3(0, -90, -180);
         yield return new WaitForSeconds(1);
 
+        // 카메라 쉐이크
+        CameraControl.Instance.OnShakeCamera(2);
+        yield return new WaitForSeconds(2);
+
         // 플레이어 startNode로 이동
         float t = 0;
         while (t < 1)
         {
-            t += Time.deltaTime * 0.01f;
-            player.position = new Vector3(player.position.x, player.position.y, player.position.z + t);
+            t -= Time.deltaTime * 0.1f;
+            player.localPosition = new Vector3(player.localPosition.x, player.localPosition.y, player.localPosition.z + t);
             yield return null;
         }
-        transform.position = new Vector3(transform.position.x, transform.position.y, 5.8f);
+        transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, 5.8f);
         yield return new WaitForSeconds(1);
 
         // column 아래로 이동
         float y = 0;
         while (y > -3.0648865f)
         {
-            y -= Time.deltaTime*0.01f;
+            y -= Time.deltaTime* 0.1f;
             column1.localPosition = new Vector3(column1.localPosition.x, column1.localPosition.y - y, column1.localPosition.z);
             column2.localPosition = new Vector3(column2.localPosition.x, column2.localPosition.y - y, column2.localPosition.z);
             yield return null;
