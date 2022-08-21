2c5c3248c8c7c226ead03af146e5de4554141ee5
suhyun <86099781+Suhyun77@users.noreply.github.com>
2022-08-21 11:48:45 +0900

    Cursor for Each Scene

--- Assets/Script/Entity/Player/PlayerInput.cs
+++ Assets/Script/Entity/Player/PlayerInput.cs
@@ -71,11 +71,6 @@ public class PlayerInput : MonoBehaviour
     /// </summary>
     public float XKeyBoardAxis { get; private set; }
 
-    /// <summary>
-    /// 클릭한 위치를 반환 (무언가에 닿았을 시)
-    /// 안닿았을 시 Vector.zero값을 반환
-    /// </summary>
-    public Vector3 ClickedPosition { get; private set; }    
     void Update()
     {
         #region 입력값 업데이트
@@ -113,11 +108,9 @@ public class PlayerInput : MonoBehaviour
         {
             //Debug.Log($"hit = {hit.collider.gameObject}");
             PointBlock = hit.collider.gameObject;
-            ClickedPosition = Camera.main.WorldToScreenPoint(hit.transform.position);
         }
         else
         {
-            ClickedPosition = Vector3.zero;
             PointBlock = null;
         }
 
