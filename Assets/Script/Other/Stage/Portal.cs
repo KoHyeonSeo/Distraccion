0158ecc9d5cbe09ff148b6ddc06682e887e765b1
Kohyeonseo <rhgustj01@naver.com>
2022-08-27 14:36:25 +0900

    Update Portal

--- Assets/Script/Other/Stage/Portal.cs
+++ Assets/Script/Other/Stage/Portal.cs
@@ -6,12 +6,35 @@ using UnityEngine.SceneManagement;
 public class Portal : MonoBehaviour
 {
     [SerializeField] private int nextStage = 0;
-
+    [SerializeField] private float distance = 3f;
+    [SerializeField] private float speed = 2f;
+    [SerializeField] private GameObject effect;
+    private bool isOnce = false;
+    private bool isStart = false;
+    private float value = 0;
+    private void Start()
+    {
+        effect.SetActive(false);
+    }
+    private void Update()
+    {
+        if (isStart)
+        {
+            value += speed * Time.deltaTime;
+            effect.transform.position += Vector3.down * speed * Time.deltaTime;
+            if(value >= distance)
+            {
+                SceneManager.LoadScene(nextStage);
+            }
+        }
+    }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && !isOnce)
         {
-            SceneManager.LoadScene(nextStage);
+            isOnce = true;
+            isStart = true;
+            effect.SetActive(true);
         }
     }
 }
