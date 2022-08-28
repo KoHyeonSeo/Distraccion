c8634967a1171634fdfe92bec2fa33bc46dbf897
suhyun <86099781+Suhyun77@users.noreply.github.com>
2022-08-28 22:59:33 +0900

    EndScene Complete

---
+++ Assets/Script/Cinemachine/DirectionAction_End2.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using Cinemachine;
+using UnityEngine.SceneManagement;
+
+public class DirectionAction_End2 : MonoBehaviour
+{
+    PlayableDirector pd;
+
+
+    [Header("다음 scene")]
+    [SerializeField] private int nextStage;
+
+
+    void Start()
+    {
+        pd = GetComponent<PlayableDirector>();
+
+    }
+
+
+    void Update()
+    {
+        // 10.5초에서 돌리카트 자식으로 vcam3 넣어주기
+        if (pd.time >= 10.5f)
+        {
+            
+        }
+
+
+        //재생시간이 다된다면
+        if (pd.duration - pd.time <= 0.05f)
+        {
+            SceneManager.LoadScene(nextStage);
+        }
+    }
+}
---
+++ Assets/Script/Entity/Player/PlayerEndAnim2.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using Cinemachine;
+
+public class PlayerEndAnim2 : MonoBehaviour
+{
+    Animator anim;
+    public PlayableDirector pd;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+
+        // 플레이어 애니메이션 초로 제어
+        if (pd.time >= 16)
+        {
+            anim.SetTrigger("Idle");
+        }
+    }
+}
